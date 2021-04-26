9641df6bfab5a999f6e27b3be4e8f7725337fe4d
Michał Granda <majcoch98@gmail.com>
2021-04-26 10:54:42 +0200

    Create solution with initial project

---
+++ slim-crypt-sln/slim-crypt-runner/main.c
@@ -0,0 +1,15 @@
+/*
+ * slim-crypt-runner.c
+ *
+ * Created: 26.04.2021 10:28:00
+ * Author : Micha³ Granda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {}
+}
+
