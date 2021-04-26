abca9c782f053acd324a6b6c5d37c2be6831b84a
Michał Granda <majcoch98@gmail.com>
2021-04-26 11:22:50 +0200

    Create empty project for AES implementation

---
+++ slim-crypt-sln/AES-algorithm/aes.c
@@ -0,0 +1,9 @@
+/*
+ * aes.c
+ *
+ * Created: 26.04.2021 11:20:36
+ *  Author: Micha³ Granda
+ */ 
+
+
+#include "aes.h"
---
+++ slim-crypt-sln/AES-algorithm/aes.h
@@ -0,0 +1,16 @@
+/*
+ * aes.h
+ *
+ * Created: 26.04.2021 11:20:25
+ *  Author: Micha³ Granda
+ */ 
+
+
+#ifndef AES_H_
+#define AES_H_
+
+
+
+
+
+#endif /* AES_H_ */
