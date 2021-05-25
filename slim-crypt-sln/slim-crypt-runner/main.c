9be798e2041ecab1f5a8aa47201714cc40270d89
Michał Granda <majcoch98@gmail.com>
2021-05-25 10:32:23 +0200

    Implement simple UART driver

--- slim-crypt-sln/slim-crypt-runner/main.c
+++ slim-crypt-sln/slim-crypt-runner/main.c
@@ -6,10 +6,21 @@
  */ 
 
 #include <avr/io.h>
+#include <stdio.h>
 
+#include "uart/uart.h"
+
+static FILE uart_str  = FDEV_SETUP_STREAM(uart_write_byte, uart_read_byte, _FDEV_SETUP_RW);
 
 int main(void) {
     /* Replace with your application code */
-    while (1) {}
+	uart_init();
+	stdout = stdin = &uart_str;
+	
+	//printf("Hello, World!");
+    while (1) {
+		uint8_t byte = uart_read_byte();
+		uart_write_byte(byte);	
+	}
 }
 
