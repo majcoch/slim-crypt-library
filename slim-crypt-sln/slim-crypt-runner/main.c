13d7e6154a0e4fe099f6e05610be0cceed1c61ae
Michał Granda <majcoch98@gmail.com>
2021-05-27 11:20:54 +0200

    Code cleanup

--- slim-crypt-sln/slim-crypt-runner/main.c
+++ slim-crypt-sln/slim-crypt-runner/main.c
@@ -10,14 +10,10 @@
 
 #include "uart/uart.h"
 
-static FILE uart_str  = FDEV_SETUP_STREAM(uart_write_byte, uart_read_byte, _FDEV_SETUP_RW);
-
 int main(void) {
     /* Replace with your application code */
 	uart_init();
-	stdout = stdin = &uart_str;
 	
-	//printf("Hello, World!");
     while (1) {
 		uint8_t byte = uart_read_byte();
 		uart_write_byte(byte);	
